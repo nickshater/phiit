454c1affaa47eb2694e1e410663f30abed51135d
Nick Shater <nicholas.shater@t1cg.com>
2017-06-01 22:32:16 -0400

    Initialize c file

--- src/c/phiit.c
+++ src/c/phiit.c
@@ -1,5 +1,27 @@
 #include <pebble.h>
 
-int main(void) {
-  app_event_loop();
+Window *window;
+TextLayer *text_layer;
+
+void init() {
+  window = window_create();
+  Layer *parent_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(parent_layer);
+  TextLayer *s_text_layer = text_layer_create(bounds);
+  text_layer_set_text(text_layer, "PHIIT");
+  layer_add_child(window_get_root_layer(window),
+         text_layer_get_layer(text_layer));
+  window_stack_push(window, true);
+}
+
+void deinit() {
+  text_layer_destroy(text_layer);
+  window_destroy(window);
+}
+
+int main() {
+   init();
+   app_event_loop();
+   deinit();
+   return 0;
 }
